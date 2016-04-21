a21140e9feebf9564187730a61a0d926a3201661
Zero <admin@admin.com>
2016-04-21 16:36:46 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# sublime
-botvs plugin for sublime
+BotVS - Sublime Plugin
+==================
+
+使用此插件Sublime可以同步本地编辑的策略
+
+### 安装方法
+
+有两种方法安装插件
+
+1. Clone the repository into your Sublime Text 2/3 packages directory.
+`git clone https://github.com/botvs/SublimeBotVS.git`
+
+2. Download the .zip file and unzip it into your Sublime Text 2/3 packages directory.
+**Note:** You can find your Sublime Text 2/3 packages directory by going to Preferences > Browse Packages.
+
+### 使用方法
+
+当你用Sublime保存带有token标识的策略源文件时, 该源文件会自动同步上传到BotVS服务器
---
+++ botvs.py
@@ -0,0 +1,93 @@
+
+# -*- coding:utf-8 -*-
+import sys
+
+import socket
+socket.setdefaulttimeout(20)
+
+import sublime, sublime_plugin
+import time, os, re
+import json
+
+try:
+    from urllib import urlopen, urlencode
+    reload(sys)   
+    sys.setdefaultencoding('utf8')
+except ImportError:
+    from urllib.request import urlopen
+    from urllib.parse import urlencode
+
+pattern = re.compile(r'botvs@([a-zA-Z0-9]{32})')
+
+token_region = 'botvs_token'
+__version__ = '0.0.1'
+rsync_url = "https://www.botvs.com/rsync"
+buf_cache = {}
+
+def getToken(view):
+    if 'JavaScript' not in view.settings().get("syntax"):
+        return (None, None)
+    view.erase_regions(token_region)
+    content = view.substr(sublime.Region(0, view.size()))
+    pos = view.find("^[ \t]*//\s*botvs@[a-zA-Z0-9]{32}[ \t]*$", 0)
+    if pos:
+        match = pattern.search(view.substr(pos))
+        if not match:
+            sublime.error_message("Invalid BotVS sync token !")
+        else:
+            view.add_regions(token_region, [pos], 'keyword', 'dot', sublime.HIDDEN)
+            view.set_status("botvs", "BotVS - sync plugin loaded")
+            content, number = re.subn("//\s*botvs@[a-zA-Z0-9]{32}\s*",'',content)
+            return (match.group(1), content)
+
+    
+    view.erase_status("botvs")
+    return (None, None)
+
+def SyncFile(filename, token, content):
+    success = False
+    errCode = 0
+    sublime.status_message("BotVS is Sync changed ....")
+    msg = ""
+    try:
+        data = {'token': token, 'method':'push', 'content': content, 'version': __version__, 'client': 'sublime ' + sublime.version()}
+        resp = json.loads(urlopen(rsync_url, urlencode(data).encode('utf8')).read().decode('utf8'))
+        errCode = resp["code"]
+        if errCode < 100:
+            success = True
+            msg = 'Hi ' + resp['user'] + ", sync success !\n\n[" + filename + "] saved to [" + resp['name'] + "]"
+            sublime.status_message(msg)
+            sublime.message_dialog(msg)
+        else:
+            if errCode == 405:
+                msg = 'Sorry, ' + resp['user'] + ", sync failed !\n\nRenew the token of [" + resp['name'] + "]"
+            elif errCode == 406:
+                msg = 'BotVS plugin for sublime need update ! \n\nhttp://www.botvs.com'
+            else:
+                msg = "BotVS sync [" + filename + " ] failed, errCode: %d\n\nMay be the token is not correct !" % errCode
+            
+    except:
+        msg = str(sys.exc_info()[1]) + "\n\nBotVS sync failed, please retry again !"
+
+    if not success:
+        sublime.status_message(msg)
+        sublime.error_message(msg)
+
+class SaveOnModifiedListener(sublime_plugin.EventListener):
+    def on_load(self, view):
+        self.token = ''
+        if getToken(view):
+            sublime.status_message("BotVS sync plugin ready .")
+
+    def on_post_save(self, view):
+        token, content = getToken(view)
+        if not token:
+            return
+        rawContent = view.substr(sublime.Region(0, view.size()))
+        if buf_cache.get(token) == rawContent:
+            sublime.error_message("BotVS sync abort because file not changed !")
+            return
+        buf_cache[token] = rawContent
+
+        file_name = os.path.basename(view.file_name())
+        SyncFile(file_name, token, content)   
